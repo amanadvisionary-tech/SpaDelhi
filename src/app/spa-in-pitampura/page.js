818fe106bc216a8981f45dd96291c76104a8daac
Amanadvisionary <aman.advisionary@gmail.com>
2026-07-31 18:19:23 +0530

    file rename

--- src/app/spa-in-pitampura/page.js
+++ src/app/spa-in-pitampura/page.js
@@ -1,5 +1,5 @@
-import Pitampurapage from "./Pitampurapage";
-
+// import pitampurapage from "./pitampurapage";
+import Pitampurapage from "./pitampurapage";
 export const metadata = {
   title: "Spa in Pitampura | Premium Massage & Wellness Services",
   description: "Discover a refined spa experience in Pitampura with expert massage, aromatherapy, and private wellness care in a luxurious setting.",
