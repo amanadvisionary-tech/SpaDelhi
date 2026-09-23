ad63ba32539f8f623f9f78d4224dd86b36e4997b
amanadvisionary-tech <aman.advisionary@gmail.com>
2026-09-23 18:02:11 +0530

    add banner image on defence colony

--- src/app/spa-in-defence-colony/DefenceColonyPage.js
+++ src/app/spa-in-defence-colony/DefenceColonyPage.js
@@ -112,7 +112,7 @@ export default function DefenceColonyPage() {
           <motion.div initial={{ opacity: 0, x: 24 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.7 }} className="relative flex items-center">
             <div className="w-full rounded-[32px] border border-amber-100 bg-white p-3 shadow-[0_20px_60px_-20px_rgba(0,0,0,0.25)]">
               <div className="relative h-[410px] overflow-hidden rounded-[24px]">
-                <Image src="/images/fpkdl.com_960_1758962686_masseur-doing-massage-woman-s-back-spa-salon-with-many-candles_355000-245.jpg" alt="Therapist giving a massage at our spa in Defence Colony" fill className="object-cover" priority />
+                <Image src="/images/sandwich-massage-3.jpg" alt="Therapist giving a massage at our spa in Defence Colony" fill className="object-cover" priority />
               </div>
             </div>
             <div className="absolute -bottom-5 -left-5 rounded-2xl border border-amber-200 bg-white/95 px-4 py-3 shadow-lg">
